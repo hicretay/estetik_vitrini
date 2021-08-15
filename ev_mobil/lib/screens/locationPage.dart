ff0bd012071c932229e552b4afa0bfdb41eb030f
Hicret Ay <46728765+hicrett@users.noreply.github.com>
2021-08-15 19:34:16 +0300

    Update locationPage.dart

--- ev_mobil/lib/screens/locationPage.dart
+++ ev_mobil/lib/screens/locationPage.dart
@@ -125,28 +125,29 @@ class _LocationPageState extends State<LocationPage> {
                                   ),),
                             child: ListTile(leading: SvgPicture.asset("assets/icons/haritanoktası.svg",color: secondaryColor,height: deviceHeight(context)*0.04),
                             title: Center(
-                              child: DropdownButton(
-                                    dropdownColor: Colors.transparent,
-                                    value: selection,
-                                    items: data.map((e){
-                                      return DropdownMenuItem(
-                                      child: Center(
-                                        child: Text(e.city,
-                                        style: TextStyle(color: white, fontSize: 20)),
-                                      ),
-                                      value: e.id.toString());
-                                    }).toList(),                                  
-                                    onChanged: (value) {
-                                     setState(() {
-                                       selection = value;
-                                     });
-                                     
-                                     },
-                                     
-                                     
-                                    ),
+                              child: DropdownButton(      
+                              dropdownColor: Colors.transparent,
+                              value: selection,
+                              items: data.map((e){
+                                return DropdownMenuItem(
+                                child: SizedBox(
+                                  width: deviceWidth(context)*0.45,
+                                  child: Center(
+                                    child: Text(e.city, textAlign: TextAlign.center,
+                                    style: TextStyle(color: white, fontSize: 20)),
+                                  ),
+                                ),
+                                value: e.id.toString());
+                              }).toList(),                                  
+                              onChanged: (value) {
+                               setState(() {
+                                 selection = value;
+                               });
+                               },
+                             ),
                             ),
-                            trailing: SvgPicture.asset("assets/icons/haritanoktası.svg",color: Colors.transparent,height: deviceHeight(context)*0.08)),
+                            trailing: SvgPicture.asset("assets/icons/haritanoktası.svg",color: Colors.transparent,height: deviceHeight(context)*0.05)
+                            ),
                         ),
                       ),
                       SizedBox(height: deviceHeight(context)*0.02),
