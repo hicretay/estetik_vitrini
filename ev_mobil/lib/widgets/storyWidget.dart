3e1e85d0508e040556fab711d679b214d71fefa8
Hicret Ay <46728765+hicrett@users.noreply.github.com>
2021-08-17 23:11:45 +0300

    170821

--- ev_mobil/lib/widgets/storyWidget.dart
+++ ev_mobil/lib/widgets/storyWidget.dart
@@ -1,6 +1,5 @@
 import 'package:estetikvitrini/model/company.dart';
 import 'package:estetikvitrini/settings/consts.dart';
-import 'package:estetikvitrini/widgets/profileWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:story_view/story_view.dart';
 
@@ -88,10 +87,42 @@ class _StoryWidgetState extends State<StoryWidget> {
               },
             ),
           ),
-          ProfileWidget(
-            company: widget.company,
-            date: date,
+        //--------------------------------Profil görünümü------------------------------------
+        Material(
+        type: MaterialType.transparency,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: deviceWidth(context)*0.05, vertical: deviceHeight(context)*0.09),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 26,
+                backgroundImage: NetworkImage(widget.company.imgUrl),
+              ),
+              SizedBox(width: deviceWidth(context)*0.02),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: maxSpace),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        widget.company.name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(date,style: TextStyle(color: Colors.white38))
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
-        ],
-      );
+        ),
+      ),
+    ],
+  );
 }
